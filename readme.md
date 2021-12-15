145ad0f64c2c6fad2c8fd1b8722d756416412485
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2021-12-15 15:56:16 +0300

    https://youtrack.jetbrains.com/issue/IDEA-284989

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 https://youtrack.jetbrains.com/issue/IDEA-277604
+
+https://youtrack.jetbrains.com/issue/IDEA-284989
+
+https://youtrack.jetbrains.com/issue/IDEA-284986
--- src/main/kotlin/com/mytests/spring/kt/mvcpathvars/MyController.kt
+++ src/main/kotlin/com/mytests/spring/kt/mvcpathvars/MyController.kt
@@ -1,18 +1,28 @@
 package com.mytests.spring.kt.mvcpathvars
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class MyController {
      @GetMapping("/ok/{pv1}/{pv2}/{pv3}")
-    fun ok(params: OkParams):String = params.toString()
+     //@GetMapping("/ok/{pv1}/{pv2}")  // no error is shown, but should https://youtrack.jetbrains.com/issue/IDEA-284989
+    fun vars(params: VarParams):String = params.toString()
 
-    data class OkParams(var pv1:String, var pv2:String, var pv3:String)
+    data class VarParams(var pv1:String, var pv2:String, var pv3:String)
     
-    @GetMapping("/error/{pv1}/{pv2}")
-    fun notOk(params: NotOkParams):String = params.toString()
+    @GetMapping("/error/{pv11}/{pv22}")
+    fun vals(params: ValParams):String = params.toString()
 
-    data class NotOkParams(val pv1:String, val pv2: String)
+    data class ValParams(val pv11:String, val pv22: String)
+
+    @GetMapping("/test/{pv111}/{pv222}/{pv333}")
+    //@GetMapping("/test/{pv111}/{pv222}")  // error is shown - ok
+    fun normal(@PathVariable pv111: String, @PathVariable pv222: String,  @PathVariable pv333: String):String{
+        return pv111+pv222+pv333
+    }
+
+
 }
